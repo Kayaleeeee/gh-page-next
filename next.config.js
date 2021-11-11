b057413cd7376dc97f527b91b2616438aa243e25
Kaya Lee <60032720+Kayaleeeee@users.noreply.github.com>
2021-11-11 10:43:06 +0900

    add image configuration

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
   assetPrefix: !isProd ? '/gh-page-next/' : '',
+  images: {
+    loader: 'imgix',
+    path: 'https://localhost:3000',
+  },
 };
