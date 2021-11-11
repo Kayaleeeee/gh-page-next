d121718fa8875dec5ce83dc532c5decb21dd20fb
Kaya Lee <60032720+Kayaleeeee@users.noreply.github.com>
2021-11-11 13:10:17 +0900

    add test page

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: !isProd ? 'https://kayaleeeee.github.io/gh-page-next' : '',
+  assetPrefix: isProd ? 'https://kayaleeeee.github.io/gh-page-next' : '',
   images: {
     loader: 'imgix',
-    path: 'https://localhost:3000',
+    path: 'https://localhost:3000/',
   },
 };
