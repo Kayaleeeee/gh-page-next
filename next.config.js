2fc77082787c1091cf443d39caadbb315bcbb6ab
Kaya Lee <60032720+Kayaleeeee@users.noreply.github.com>
2021-11-11 12:14:27 +0900

    modify assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: !isProd ? '/gh-page-next/' : '',
+  assetPrefix: !isProd ? 'https://kayaleeeee.github.io/gh-page-next' : '',
   images: {
     loader: 'imgix',
     path: 'https://localhost:3000',
